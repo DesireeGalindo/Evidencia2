e2bb05c135b17ed2335a841b2caeb09da96b9baf
Desiree <A01669727@tec.mx>
2025-09-20 00:32:28 -0600

    Parte 2 de Evidencia

--- calcularFig.py
+++ calcularFig.py
@@ -2,11 +2,25 @@ import pandas as pd
 from figuras import triangulo, circulo, rectangulo
 
 
-
-df = pd.read_csv("figuras.csv") #lee el archivo
+df = pd.read_csv("figuras.csv")  # lee el archivo
 print(df)
 
 
 for index, row in df.iterrows():
-	print(f"Fila {index}; FIGURA={row['FIGURA']}, MEDIDA1={row['MEDIDA1']}, MEDIDA2={row['MEDIDA 2']}")
+    figura = row['FIGURA']
+    base = row['MEDIDA1']
+    altura = row['MEDIDA2']
+
+    # calculamos según la figura
+    if figura == 't':  # triángulo
+        area, perimetro = triangulo(base, altura)
+    elif figura == 'c':  # círculo
+        area, perimetro = circulo(base) #es radio pero lo tomamos como base
+    elif figura == 'r':  # rectángulo
+        area, perimetro = rectangulo(base, altura)
+    else:
+        area, perimetro = None, None
+
+    # print final con todo
+    print(f"Fila {index}; FIGURA={figura}, MEDIDA1={base}, MEDIDA2={altura}, AREA={area}, PERIMETRO={perimetro}")
 
