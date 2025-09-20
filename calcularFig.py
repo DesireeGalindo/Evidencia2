e1672b744f77aabc5bdf3a28057008ac95ee7061
Desiree <A01669727@tec.mx>
2025-09-19 22:24:59 -0600

    Actualizar scripts y agregar dataset figuras.csv

--- calcularFig.py
+++ calcularFig.py
@@ -1,5 +1,12 @@
 import pandas as pd
+from figuras import triangulo, circulo, rectangulo
 
-df = pd.read.csv("figuras.csv")
-print("el archivo ha sido leído")
+
+
+df = pd.read_csv("figuras.csv") #lee el archivo
+print(df)
+
+
+for index, row in df.iterrows():
+	print(f"Fila {index}; FIGURA={row['FIGURA']}, MEDIDA1={row['MEDIDA1']}, MEDIDA2={row['MEDIDA 2']}")
 
